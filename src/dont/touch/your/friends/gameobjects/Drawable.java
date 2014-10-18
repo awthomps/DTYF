436e297ec4377ed892e8c00d63353bc3b166d5f8
Andrei <killosquirrel@gmail.com>
2014-10-18 03:17:48 -0700

    some untested collision detection code

--- src/dont/touch/your/friends/gameobjects/Drawable.java
+++ src/dont/touch/your/friends/gameobjects/Drawable.java
@@ -19,4 +19,24 @@ public abstract class Drawable {
 	public void setPosition(float x, float y) {
 		pos.set(x, y);
 	}
+	
+	public void setRect(int x, int y) {
+		rect = new Rectangle(x, y);
+	}
+	
+	public void setRect(Rectangle r) {
+		rect = r;
+	}
+	
+	public boolean collidesWith(Drawable d) {
+		if((pos.getX() + rect.width < d.pos.getX()) && (pos.getY() + rect.height < d.pos.getY())) return false;
+		else if((pos.getX() + rect.width < d.pos.getX()) && (pos.getY() > d.pos.getY() + d.rect.height)) return false;
+		else if((pos.getX() > d.pos.getX() + d.rect.width) && (pos.getY() > d.pos.getY() + d.rect.height)) return false;
+		else if((pos.getX() > d.pos.getX() + d.rect.width) && (pos.getY() + rect.height < d.pos.getY())) return false;
+		else return true;
+	}
+
+	public void handleCollision(Drawable d2) {
+		System.out.println("Hey look, " + this + " collides with " + d2);
+	}
 }
