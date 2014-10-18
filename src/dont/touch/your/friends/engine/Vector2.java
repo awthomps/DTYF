e559c53e35e10a63127d662d7e4635b9638937dc
Andrei <killosquirrel@gmail.com>
2014-10-18 12:18:53 -0700

    to string stuff for vector2

--- src/dont/touch/your/friends/engine/Vector2.java
+++ src/dont/touch/your/friends/engine/Vector2.java
@@ -55,4 +55,11 @@ public class Vector2 {
 		v[0] = x;
 		v[1] = y;
 	}
+	
+	public String toString() {
+		return "Vector2( " + v[0] + ", " + v[1] + ")";
+	}
+	public void print() {
+		System.out.println(toString());
+	}
 }
